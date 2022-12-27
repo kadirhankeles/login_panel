8883fbd7e0b6f158ad4fa17e3214212d31c183f8
Kadirhan Keles <kadirhankeles@gmail.com>
2022-12-27 12:48:14 +0300

    commit

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -2,26 +2,25 @@ import 'package:get/get.dart';
 
 import '../../shared/service/firestore_service.dart';
 
-class HomeController extends GetxController{
-  FirestoreService store =FirestoreService();
-  RxList blogList =[].obs;
-  
-  
+class HomeController extends GetxController {
+  FirestoreService store = FirestoreService();
+  RxList blogList = [].obs;
+
   bool isLoading = false;
   @override
   void onInit() async {
     super.onInit();
-    print("Çalıştı!!!!!");
     isLoading = false;
-    await store.getTextList();
-    store.itemList.forEach((element) {blogList.add(element); });
-    isLoading=true;
+    blogList.value = (await store.getTextList())!;
+    isLoading = true;
   }
- 
 
-  
+  getList() async {
+    blogList.value = (await store.getTextList())!;
+  }
 
-
-  
-  
+  Future removeText(docId) async {
+    //await store.removeText(docId);
+    store.itemList.removeWhere((element) => element['documentid'] == docId);
+  }
 }
